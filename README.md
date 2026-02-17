1a5aaa0d5bedca2ae3e65099dbc5414f9f872cc2
Felix Chen <felixchen531@gmail.com>
2026-02-17 16:05:19 +0000

    Feature: Add backend main entry

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A marketplace for university people
 # Development
 
 ## Requirements
-- Python 3.13 (w/ [uv](https://docs.astral.sh/uv/))
+- Python 3.14 (w/ [uv](https://docs.astral.sh/uv/))
 - Node.js 24 (LTS Recommended)
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from uomarket!")
+import sys
 
+from loguru import logger
+
+from src import app
+from src import config
 
 if __name__ == "__main__":
-    main()
+    logger.remove()
+    temp_logger = logger.add(sys.stderr, level="DEBUG")
+    logger.debug("Starting UoMarket")
+
+    try:
+        logger.debug("Loading configuration")
+        app_conf = config.AppConfig.load_config("example/config.yaml")
+        logger.debug("Config loaded")
+    except Exception as e:
+        logger.error(e)
+        sys.exit(1)
+
+    logger.remove(temp_logger)
+    logger.add(sys.stderr, level=app_conf.log.level)
+    logger.info(f"Log level set to {app_conf.log.level}")
+
+    app.run(app_conf)
