7972a889635a61bda35990ded4a35e8305296ccc
Felix Chen <felixchen531@gmail.com>
2026-02-24 07:35:44 +0000

    Add django router/middleware/view, uppdate main.ts for csrf api calling 	modified:   .gitignore 	renamed:    example/config.yaml -> config.yaml 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/main.ts 	modified:   pyproject.toml 	modified:   uv.lock 	docs/backend/API.md 	docs/backend/Web.md 	example.yaml 	src/storage/ 	src/web/

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue';
 import router from './router';
 import './assets/main.css';
 
+// Set up CSRF token when calling Django API
+import axios from 'axios';
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
