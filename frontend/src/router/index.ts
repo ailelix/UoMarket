eebdc71ba65324ca823c37649a5db97e4f0354df
Aaron <aaron.mathew@student.manchester.ac.uk>
2026-03-02 21:11:06 +0000

    Removed Register button and router link

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
-import Register from '../views/Register.vue';
 import MarketPlace from '../views/Marketplace.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path : '/login', name: 'Login', component: Login },
-  { path : '/register', name: 'Register', component: Register },
   { path : '/marketplace', name: 'MarketPlace', component: MarketPlace },
   
 ];
