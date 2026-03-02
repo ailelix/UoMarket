f05eb4084da5c1e7ed295c84ce2c089220c50727
Aaron <aaron.mathew@student.manchester.ac.uk>
2026-03-02 21:07:27 +0000

    implemented basic profile view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,12 +3,14 @@ import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import MarketPlace from '../views/Marketplace.vue';
+import ProfileView from '../views/ProfileView.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path : '/login', name: 'Login', component: Login },
   { path : '/register', name: 'Register', component: Register },
   { path : '/marketplace', name: 'MarketPlace', component: MarketPlace },
+  { path : '/profile', name: 'ProfileView', component: ProfileView },
   
 ];
 
